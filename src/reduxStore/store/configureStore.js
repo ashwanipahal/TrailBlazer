db97e4d55cbc07c48174fa2cf59e19c8faf70629
Ashwani Payal <ashwani.payal@publicissapient.com>
2021-02-25 18:28:58 +0530

    redux fixes

--- src/reduxStore/store/configureStore.js
+++ src/reduxStore/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import globalSagas from '../sagas';
 import globalReducers from '../reducers';
