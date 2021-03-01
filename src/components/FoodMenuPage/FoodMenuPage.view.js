cf3e24d5a7cf289b8f240527549152b22116a6ed
Ashwani Payal <ashwani.payal@publicissapient.com>
2021-03-01 22:02:52 +0530

    entry added map

--- src/components/FoodMenuPage/FoodMenuPage.view.js
+++ src/components/FoodMenuPage/FoodMenuPage.view.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react';
 
 const FoodMenuView = (props) => {
-  const { foodMenu,getFoodMenu } = props;
+  const { foodMenu, getFoodMenu } = props;
 
-  useEffect(()=>{
-    getFoodMenu(); 
-  },[])
+  useEffect(() => {
+    getFoodMenu();
+  }, []);
 
   return (
-    <div className='food-menu-wrapper'><h3>Here is the food menu available in Ground floor cafeteria by Royal Grillz.</h3><img src={foodMenu} /> </div>   
+    <div className='food-menu-wrapper'>
+      <h3>Here is the food menu available in Ground floor cafeteria by Royal Grillz.</h3>
+      <img src={foodMenu} />{' '}
+    </div>
   );
-};  
+};
 
 export default FoodMenuView;
