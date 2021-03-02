978e547dca90aaddc21938a6c0f559fd90c8ae40
Ashwani Payal <ashwani.payal@publicissapient.com>
2021-03-02 21:37:02 +0530

    backend code updated

---
+++ demo-trailblaze/src/main/java/com/trailblazer/demo/models/SeatInfo.java
@@ -0,0 +1,49 @@
+package com.trailblazer.demo.models;
+
+/**
+ * The Class SeatInfo.
+ */
+public class SeatInfo {
+    
+    /** The id. */
+    private String id;
+    
+    /** The lat long. */
+    private String latLong;
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the lat long.
+	 *
+	 * @return the lat long
+	 */
+	public String getLatLong() {
+		return latLong;
+	}
+
+	/**
+	 * Sets the lat long.
+	 *
+	 * @param latLong the new lat long
+	 */
+	public void setLatLong(String latLong) {
+		this.latLong = latLong;
+	}
+}
